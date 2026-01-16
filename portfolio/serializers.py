2ee14555fca12a3769bce169a3644983bdd12f36
Dominic62q <dominicquainoo62@gmail.com>
2026-01-16 13:24:53 +0000

    Add absolute URL support for project images in API

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -20,6 +20,16 @@ class ContactMessageSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         model = Project
         fields = "__all__"
+    
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            if request:
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
+        return None
